98c4d45ceea98f43b0603d78e5cdccca065d16c9
Justin Ugerio <justinugerio@users.noreply.github.com>
2015-07-17 13:50:00 -0700

    Code through some examples.
    Signed-off-by: Justin Ugerio <justinugerio@users.noreply.github.com>

--- client/js/mapper.js
+++ client/js/mapper.js
@@ -2,7 +2,66 @@
 // main start function
 $(function () {
 
+    /*
     console.log('hello there!');
 
+    d3.select('#mapper-panel-body-id').append('div')
+        .style('border', '1px black solid')
+        .html('hello world');
+
+    d3.select('#mapper-panel-body-id').select('div')
+        .style('background-color', 'orange')
+        .style('font-size', '24px')
+        .attr('id', 'newDiv')
+        .attr('class', 'd3div')
+        .on('click', function () {
+            console.log('you click on a div!!!');
+        })
+
+    d3.select('svg')
+        .append('line')
+        .attr('x1', 20)
+        .attr('y1', 20)
+        .attr('x2', 400)
+        .attr('y2', 400)
+        .style('stroke', 'black')
+        .style('stoke-width', '2px');
+     */
+
+
+    d3.select('svg')
+        .append('circle')
+        .attr('r', 20)
+        .attr('cx', 20)
+        .attr('cy', 20)
+        .style('fill', 'red');
+
+    d3.select('svg')
+        .append('text')
+        .attr('x', 20)
+        .attr('y', 20)
+        .text('Hello there!!!')
+        .style('opacity', 0)
+        .attr('id', 'a');
+
+    d3.select('svg')
+        .append('circle')
+        .attr('r', 100)
+        .attr('cx', 400)
+        .attr('cy', 400)
+        .style('fill', 'lightblue');
+
+    d3.select('svg')
+        .append('text')
+        .attr('x', 400)
+        .attr('y', 400)
+        .text('Uh, hi!!!')
+        .attr('id', 'b')
+        .style('opacity', 0);
+
+    d3.select('#a').transition().delay(1000).style('opacity', 1);
+    d3.select('#b').transition().delay(4000).style('opacity', 0.75);
+
+    d3.selectAll('circle').transition().duration(3000).attr('cy', 200);
 
 });
