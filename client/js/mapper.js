9cd305d3c802b6479b2bab0edd51866873e77220
Justin Ugerio <justinugerio@users.noreply.github.com>
2015-07-18 08:58:14 -0700

    Added points to map
    Signed-off-by: Justin Ugerio <justinugerio@users.noreply.github.com>

--- client/js/mapper.js
+++ client/js/mapper.js
@@ -2,60 +2,38 @@
 // main start function
 $(function () {
 
-/*
-    d3.select('svg')
-        .append('circle')
-        .attr('r', 20)
-        .attr('cx', 20)
-        .attr('cy', 20)
-        .style('fill', 'red');
+    var createMap = function (countries, cities) {
+        var width = 500;
+        var height = 500;
+        var projection = d3.geo.mercator()
+            .scale(110);
+        var geoPath = d3.geo.path().projection(projection);
 
-    d3.select('svg')
-        .append('text')
-        .attr('x', 20)
-        .attr('y', 20)
-        .text('Hello there!!!')
-        .style('opacity', 0)
-        .attr('id', 'a');
-
-    d3.select('svg')
-        .append('circle')
-        .attr('r', 100)
-        .attr('cx', 400)
-        .attr('cy', 400)
-        .style('fill', 'lightblue');
-
-    d3.select('svg')
-        .append('text')
-        .attr('x', 400)
-        .attr('y', 400)
-        .text('Uh, hi!!!')
-        .attr('id', 'b')
-        .style('opacity', 0);
-
-    d3.select('#a').transition().delay(1000).style('opacity', 1);
-    d3.select('#b').transition().delay(4000).style('opacity', 0.75);
-
-    d3.selectAll('circle').transition().duration(3000).attr('cy', 200);
-
-
-     d3.csv('cities.csv', function (d) {
-        console.log(d);
-     });
-*/
-
-    var createMap = function (countries) {      // create map handler for getJSON call
-        //var aProjection = d3.geo.albersUsa();
-        var aProjection = d3.geo.mercator().scale(100);
-        var geoPath = d3.geo.path().projection(aProjection);
-
-        d3.select('svg').selectAll('path').data(countries.features)
+        d3.select("svg").selectAll("path").data(countries.features)
             .enter()
-            .append('path')
-            .attr('d', geoPath)
+            .append("path")
+            .attr("d", geoPath)
             .attr('class', 'countries');
-    };
 
-    $.getJSON('world.geojson', createMap);  // asynchronous call to get data in world.geojson
+        d3.select("svg").selectAll("circle").data(cities)
+            .enter()
+            .append("circle")
+            .style("fill", "red")
+            .attr("class", "cities")
+            .attr("r", 3)
+            .attr("cx", function (d) {
+                return projection([d.x, d.y])[0]
+            })
+            .attr("cy", function (d) {
+                return projection([d.x, d.y])[1]
+            });
+    }
+
+    d3.csv('cities.csv', function(cities) { // async call to get data in cities.csv
+        $.getJSON('world.geojson', function(countries) {    // async call to get data in world.geojson
+            createMap(countries, cities);   // when both async functions return, call create Map
+        });
+    });
+
 
 });
