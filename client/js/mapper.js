4e9296e0fb388d9f2ec26ae50695f7dd601ee9c0
Justin Ugerio <justinugerio@users.noreply.github.com>
2015-07-20 00:44:50 -0700

    Embedded map from Mapbox via mapbox.js
    Signed-off-by: Justin Ugerio <justinugerio@users.noreply.github.com>

--- client/js/mapper.js
+++ client/js/mapper.js
@@ -2,6 +2,14 @@
 // main start function
 $(function () {
 
+    // mapbox.js example
+    L.mapbox.accessToken = 'pk.eyJ1IjoianVzdGludSIsImEiOiI3MjA5MjhmZTA0ZGMyYjMzMzE5OWQwZmZkZWE5YTU4MiJ9.voJN-SvlJY5QuW1Qj6d_Gw';
+    var map = L.mapbox.map('map', 'justinu.moonmnfj'); // '' or 'mapbox.streets'
+        //.setView([33.7, -117.8], 15);
+
+
+    /*
+    // d3.js example of the map of the world
     var createMap = function (countries, cities) {
         var width = 500;
         var height = 500;
@@ -29,11 +37,12 @@ $(function () {
             });
     }
 
+
     d3.csv('cities.csv', function(cities) { // async call to get data in cities.csv
         $.getJSON('world.geojson', function(countries) {    // async call to get data in world.geojson
             createMap(countries, cities);   // when both async functions return, call create Map
         });
     });
-
+    */
 
 });
